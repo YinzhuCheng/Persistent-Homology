973f6ac64cf6e9ed7bf0c7577e99767a0c2f682d
YinzhuCheng <165536836+YinzhuCheng@users.noreply.github.com>
2025-08-29 15:46:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  base: "./",            // ★ 关键修改：让构建后的 index.html 使用相对路径
+  plugins: [react()],
 });
